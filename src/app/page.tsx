db4777e1a9c2409265c268fcd8d2079f06ab842f
Marina Crespo <marina.crespo@valtech.com>
2023-12-15 16:18:46 -0300

    desktop home page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,16 @@ export default function page() {
   return (
     <div className={styles.main}>
       <Navbar/>
-      <div className={styles.textContainer}>
-        <h1 className={styles.littleText}>SO YOU WANT TO TRAVEL TO</h1>
-        <h2 className={styles.bigText}>SPACE</h2>
-        <p className={styles.paragraph}>
-        Let’s face it; if you want to go to space,
-        you might as well genuinely go to outer space and not hover kind of on the edge of it.
-        Well sit back, and relax because we’ll give you a truly out of this world experience!
-        </p>
+      <div className={styles.mainContainer}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.littleText}>SO YOU WANT TO TRAVEL TO</h1>
+          <h2 className={styles.bigText}>SPACE</h2>
+          <p className={styles.paragraph}>
+          Let’s face it; if you want to go to space,
+          you might as well genuinely go to outer space and not hover kind of on the edge of it.
+          Well sit back, and relax because we’ll give you a truly out of this world experience!
+          </p>
+        </div>
         <button type='button' className={styles.button}>
           EXPLORE
         </button>
---
+++ src/constants/breakpoints.tsx
@@ -0,0 +1,5 @@
+export const breakpoints = {
+    mobile: 767,
+    tablet: 1023,
+    desktop: 1280
+}
